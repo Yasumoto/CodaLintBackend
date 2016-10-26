ca6ef8f06600bb3a930b173eaf9bac57abf5b1d0
Joe Smith <jmsmith@slack-corp.com>
2016-10-26 14:45:31 -0700

    Initial Commit

---
+++ CodaLintBackend/CodaLintBackend.h
@@ -0,0 +1,19 @@
+//
+//  CodaLintBackend.h
+//  CodaLintBackend
+//
+//  Created by Joseph Mehdi Smith on 10/26/16.
+//  Copyright © 2016 Joe Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CodaLintBackend.
+FOUNDATION_EXPORT double CodaLintBackendVersionNumber;
+
+//! Project version string for CodaLintBackend.
+FOUNDATION_EXPORT const unsigned char CodaLintBackendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodaLintBackend/PublicHeader.h>
+
+
